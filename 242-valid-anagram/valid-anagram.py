3b0f68d8744757c6440bb6bdf4c58e8e851574cc
Darwin J <67301026+darwin-j@users.noreply.github.com>
2023-11-22 06:36:49 +0530

    Time: 36 ms (50.31%) | Memory: 13.7 MB (76.29%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        s_freq = {}
+        t_freq = {}
+        for char in s:
+            s_freq[char] = s_freq.get(char, 0) + 1
+        for char in t:
+            t_freq[char] = t_freq.get(char, 0) + 1
+
+        return s_freq == t_freq
+        
+    
+        
