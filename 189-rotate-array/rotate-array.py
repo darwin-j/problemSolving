7005d8295f4a5ef658ff52c727575b71d56b2b92
Darwin J <67301026+darwin-j@users.noreply.github.com>
2023-11-22 06:18:21 +0530

    Time: 181 ms (68.62%) | Memory: 27.9 MB (17.05%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,8 +1,14 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         lengthOfNums = len(nums)
-        for i in range(k%lengthOfNums):
-            lastNum = nums[lengthOfNums-1]
-            nums.pop()
-            nums.insert(0,lastNum)
+        k = k%lengthOfNums
+        def rotateArr(start,end):
+            while start < end:
+                nums[start],nums[end] = nums[end],nums[start]
+                start += 1
+                end -= 1
+        if k != 0:
+            rotateArr(0,lengthOfNums-1)
+            rotateArr(0,k-1)
+            rotateArr(k,lengthOfNums-1)
         
