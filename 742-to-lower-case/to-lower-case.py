999d55e0bd9e62f472e681aa5cc5d4214377631a
Darwin J <67301026+darwin-j@users.noreply.github.com>
2024-02-10 06:33:42 +0530

    Time: 29 ms (91.32%) | Memory: 16.5 MB (60.20%) - LeetSync

--- 742-to-lower-case/to-lower-case.py
+++ 742-to-lower-case/to-lower-case.py
@@ -1,7 +1,5 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        # find if letter's asii code is less then a then it is upper case
-        # add 32 to get lower case
         string_list = list(s) 
         for index,letter in enumerate(string_list):
             if ord(letter) >= 65 and ord(letter) <= 90:
