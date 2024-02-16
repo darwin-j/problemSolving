cdf9ed8554a24d24fcc1d5c46516cb1ad1976f9a
Darwin J <67301026+darwin-j@users.noreply.github.com>
2024-02-16 06:48:40 +0530

    Time: 34 ms (97.15%) | Memory: 17.4 MB (67.60%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        onlyAlphhaString = []
+        s = s.strip()
+        for char in s:
+            asscii_of_char = ord(char)
+            if (asscii_of_char >= 97 and asscii_of_char <= 122) or char.isdigit():
+                onlyAlphhaString.append(char)
+            elif(asscii_of_char >= 65 and asscii_of_char <= 90):
+                onlyAlphhaString.append(chr(asscii_of_char+32))
+        onlyAlphhaString = ''.join(onlyAlphhaString)
+        print(onlyAlphhaString)
+        if len(onlyAlphhaString)==0:
+            return True
+        else:
+            return True if onlyAlphhaString == onlyAlphhaString[::-1] else False
+        
